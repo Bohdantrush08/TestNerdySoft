69f32a3b786dec9e9a0c338ceca6a57e9f845f42
Bohdantrush08 <32901504+Bohdantrush08@users.noreply.github.com>
2019-10-21 22:55:08 +0300

    Initial commit

---
+++ src/main/java/com/trush/TestNerdySoft/TestNerdySoftApplication.java
@@ -0,0 +1,13 @@
+package com.trush.TestNerdySoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestNerdySoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestNerdySoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trush/TestNerdySoft/TestNerdySoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trush.TestNerdySoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestNerdySoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
